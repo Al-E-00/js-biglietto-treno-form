63e1c4dec33bd40e59a230f9778a1607cc24a43c
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-20 17:43:01 +0200

    error undefined value in price

--- js/action.js
+++ js/action.js
@@ -14,21 +14,50 @@ Al click sul tasto ANNULLA si svuotano i campi
 
 /* Inizio con il prendere le variabili dal documento HTML */
 
-const inputNameSurname = document.getElementById("nameSurname");
-const inputKm = document.getElementById("km");
-const inputAge = document.getElementById("age");
+const inputUserName = document.querySelector(`[name="userName"]`);
+const inputUserKm = document.querySelector(`[name="userKm"]`);
+const inputUserAge = document.querySelector(`[name="userAge"]`);
 
 const submitGenera = document.getElementById("submitGenera");
 const submitAnnulla = document.getElementById("submitAnnulla");
 
-const insertName = document.getElementById("nameSurnameIns");
+
 /* Se si clicca sul pulsante Genera devo salvare i dati nelle variabili */
 
-/* Con il pulsante annulla vanno svuotati i campi */
 
 submitGenera.addEventListener("click", function() {
+    /* Questo codice verrà eseguito al click del pulsante */
+    
+    console.log(inputUserName.value, inputUserKm.value, inputUserAge.value);
+    
 
-    console.log(inputNameSurname.value, inputKm.value, inputAge.value);
+    /* Per inserire i dati dalla stringa superiore a quella inferiore */
+    document.getElementById("nameSurnameIns").innerHTML = inputUserName.value;
+    
+    // calcolo del prezzo del biglietto intero
+    let ticketPrice = parseInt(inputUserKm.value) * 0.21;
+    console.log (ticketPrice);
+    // applicare sconti specifici in base all'età
+    let age = parseInt(inputUserAge.value);
 
-    inputNameSurname.textContent = insertName.value
+    if (age > 14 && age < 18) {
+        ticketPrice = ticketPrice - (ticketPrice * 0.20);
+    } else if (age > 65){
+        ticketPrice = ticketPrice - (ticketPrice * 0.40);
+    } else {
+        ticketPrice;
+    }
+
+    console.log(ticketPrice);
+    // stampare il prezzo sul biglietto nuovo
+
+    document.querySelector(".ticketPriceIntoCard").innerHTML = ticketPrice.value;
+
+    
 })
+
+
+
+
+
+/* Con il pulsante annulla vanno svuotati i campi */
